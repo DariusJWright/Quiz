89b35dceef0d3220fc5d7f66fa47a14f1f9ad46f
darshnablah <dwright758@gmail.com>
2020-06-13 23:12:50 -0500

    setting marker

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,28 @@
 var seconds = 75;
+
 var correct = document.createElement("p");
-    correct.textContent = "Correct!"
-    correct.style.textEmphasis = true;
+    correct.innerHTML = "<em>Correct!</em>"
+    correct.style.textAlign = "left";
+    correct.style.marginLeft = "100px";
+    correct.style.marginRight = "100px";
+    correct.style.marginTop = "10px";
+    correct.style.borderTop = "solid 2px black";
+    correct.style.paddingTop = "5px";
 
 var wrong = document.createElement("p");
-    wrong.textContent = "Wrong!"
-    wrong.style.textEmphasis = true;
+    wrong.innerHTML = "<em>Wrong!</em>"
+    wrong.style.textAlign = "left";
+    wrong.style.marginLeft = "100px";
+    wrong.style.marginRight = "100px";
+    wrong.style.marginTop = "10px";
+    wrong.style.borderTop = "solid 2px black";
+    wrong.style.paddingTop = "5px";
+
+var answers = ["Booleans", "Numbers", "Strings", "Alerts"];
+
+var questions = ["Commonly used data types do not include:", "The condition in an if/else statement is enclosed with:", "Arrays in JavaScript can be used to store:"]
+
+
 
 var timer = function() {
     
@@ -37,81 +54,46 @@ var intro = function(){
     question.appendChild(button);
 
     var start = document.querySelector("#start-button");
-    start.addEventListener("click", timer);
-    start.addEventListener("click", question1El);
     
+    start.addEventListener("click", timer);
+    start.addEventListener("click", quiz1);  
 }
 
-var question1El = function() {
-    var question = document.getElementById("question");
-        question.textContent = "Commonly used data types DO NOT include:";
-    var buttonContainerEL = document.getElementById("button-container");
+var quiz1 = function() {
+    var questionEl = document.getElementById("question");
+    questionEl.className = "question";
+    var answer = document.getElementById("button-container");
 
-    var button1 = document.createElement("button");
-        button1.className = "button";
-        button1.textContent = "1. booleans";
-        buttonContainerEL.appendChild(button1);
-
-    var button2 = document.createElement("button");
-        button2.className = "button";
-        button2.textContent = "2. numbers";
-        buttonContainerEL.appendChild(button2);
-
-    var button3 = document.createElement("button");
-        button3.className = "button";
-        button3.textContent = "3. strings";
-        buttonContainerEL.appendChild(button3);
-
-    var button4 = document.createElement("button");
-        button4.className = "button";
-        button4.textContent = "4. alerts";
-        buttonContainerEL.appendChild(button4);
-
-    if (button4) {
-        question2EL();
-        buttonContainerEL.appendChild(correct);
+    for (var i = 0; i < questions.length; i++) {
+        question = questions[i];
+        questionEl.textContent = question;
     }
 
-    else {
-        question2EL();
-        buttonContainerEL.appendChild(wrong);
-    }    
-}
-
-var question2El = function() {
-    var question = document.getElementById("question");
-        question.textContent = "Commonly used data types DO NOT include:";
-    var buttonContainerEL = document.getElementById("button-container");
-
-    var button1 = document.createElement("button");
-        button1.className = "button";
-        button1.textContent = "1. booleans";
-        buttonContainerEL.appendChild(button1);
-
-    var button2 = document.createElement("button");
-        button2.className = "button";
-        button2.textContent = "2. numbers";
-        buttonContainerEL.appendChild(button2);
-
-    var button3 = document.createElement("button");
-        button3.className = "button";
-        button3.textContent = "3. strings";
-        buttonContainerEL.appendChild(button3);
-
-    var button4 = document.createElement("button");
-        button4.className = "button";
-        button4.textContent = "4. alerts";
-        buttonContainerEL.appendChild(button4);
-
-    if (button4) {
-        question2EL();
-        buttonContainerEL.appendChild(correct);
+    for (var i = 0; i < answers.length; i++) {
+        var choice = document.createElement("button");
+        choice.className = "button";
+        choice.id = "button" + [i];
+        choice.textContent = [i + 1] + ". " + answers[i];
+        answer.appendChild(choice);
+        console.log(choice);
     }
+    
+    var correctAnswerEl = document.querySelector("#button-container");
+    correctAnswerEl.addEventListener("click", console.log(choice));
+    
+    
 
-    else {
-        question2EL();
-        buttonContainerEL.appendChild(wrong);
-    }    
+    var select = function(event) {
+        var correctAnswer = event.target.getAttribute("id");
+        console.log(correctAnswer);
+        if (correctAnswer.matches("button3")) {
+            answer.appendChild(correct);
+            console.log(3);
+        }
+        else {
+            answer.appendChild(wrong);
+        }
+    }
 }
 
 
