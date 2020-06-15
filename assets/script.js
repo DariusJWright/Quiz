e5f0530236af32ee4215eff6d8d4222c80f23ed8
darshnablah <dwright758@gmail.com>
2020-06-14 19:27:51 -0500

    done-ish

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ var startButtonDiv = document.querySelector("#start");
 var buttonContainer = document.querySelector("#button-container");
 var response = document.querySelector("#response");
     response.className = "response";
+var score = 0;
 
 var answers1 = ["Booleans", "Numbers", "Strings", "Alerts"];
 var answers2 = ["Quotes", "Curly brackets", "Parenthesis", "Square brackets"];
@@ -23,13 +24,13 @@ var timer = function() {
         seconds--;
         
         countdown.textContent = "Time: " + seconds;
-        if (seconds <= 0 || ) {
+        if (seconds <= 0) {
             countdown.textcontent = "";
             clearInterval(timeLeft);
             endQuiz();
         }
     }, 1000);
-    
+    score = seconds;
     quiz1();
 }
 
@@ -44,14 +45,12 @@ var quiz1 = function() {
         choice.id = "button" + [i];
         choice.textContent = [i + 1] + ". " + answers1[i];
         buttonContainer.appendChild(choice);
-        console.log(choice);
     }
     buttonContainer.addEventListener("click", select1);
 }    
 
 var select1 = function(event) {
     var correctAnswer = event.target.getAttribute("id");
-    console.log(correctAnswer);
     
     if (correctAnswer === "button3") {
         quiz2();
@@ -59,7 +58,6 @@ var select1 = function(event) {
     }
     else {
         seconds -= 10;
-        console.log(seconds, "select1 else");
         quiz2();
         response.innerHTML = "<em>Wrong!</em>";
     }
@@ -67,7 +65,6 @@ var select1 = function(event) {
 
 var quiz2 = function() {
     buttonContainer.removeEventListener("click", select1);
-    console.log(seconds, "is in quiz2");
     intro.textContent = "";
     startButtonDiv.innerHTML = "";
     questionEl.textContent = questions[1];
@@ -79,14 +76,12 @@ var quiz2 = function() {
         choice.id = "button" + [i];
         choice.textContent = [i + 1] + ". " + answers2[i];
         buttonContainer.appendChild(choice);
-        console.log(choice);
     }
     buttonContainer.addEventListener("click", select2);
 }    
 
 var select2 = function(event) {
     var correctAnswer = event.target.getAttribute("id");
-    console.log(correctAnswer);
     if (correctAnswer === "button2") {
         quiz3();
         response.innerHTML = "<em>Correct!</em>";
@@ -111,14 +106,12 @@ var quiz3 = function() {
         choice.id = "button" + [i];
         choice.textContent = [i + 1] + ". " + answers3[i];
         buttonContainer.appendChild(choice);
-        console.log(choice);
     }
     buttonContainer.addEventListener("click", select3);
 }    
 
 var select3 = function(event) {
     var correctAnswer = event.target.getAttribute("id");
-    console.log(correctAnswer);
     if (correctAnswer === "button3") {
         response.innerHTML = "<em>Correct!</em>";
         quiz4();
@@ -143,14 +136,12 @@ var quiz4 = function() {
         choice.id = "button" + [i];
         choice.textContent = [i + 1] + ". " + answers4[i];
         buttonContainer.appendChild(choice);
-        console.log(choice);
     }
     buttonContainer.addEventListener("click", select4);
 }    
 
 var select4 = function(event) {
     var correctAnswer = event.target.getAttribute("id");
-    console.log(correctAnswer);
     if (correctAnswer === "button1") {
         response.innerHTML = "<em>Correct!</em>";
         quiz5();
@@ -175,14 +166,12 @@ var quiz5 = function() {
         choice.id = "button" + [i];
         choice.textContent = [i + 1] + ". " + answers5[i];
         buttonContainer.appendChild(choice);
-        console.log(choice);
     }
     buttonContainer.addEventListener("click", select5);
 }    
 
 var select5 = function(event) {
     var correctAnswer = event.target.getAttribute("id");
-    console.log(correctAnswer);
     if (correctAnswer === "button3") {
         response.innerHTML = "<em>Correct!</em>";
         endQuiz();
@@ -201,7 +190,25 @@ var endQuiz = function() {
     intro.textContent = "Your final score is " + seconds + ".";
     start.innerHTML = "";
     document.getElementById("button-container").remove();
-    response.innerHTML = "<p>Enter initials:</p><input></input><button>submit</button>";
+    response.innerHTML = "<p>Enter initials:</p>";
+    var input = document.createElement("input");
+    input.className = "input";
+    var submit = document.createElement("button");
+    submit.className = "submit";
+    submit.textContent = "Submit";
+    response.appendChild(input);
+    response.appendChild(submit);
+    
+    
+    submit.addEventListener("click", hs);
 }
+
+
+var hs = function () {
+    var initialInput = document.querySelector("input[class-name='input']").value;
+    initialInput = JSON.stringify(initialInput);
+    localStorage.setItem(initialInput, seconds);
+}
+
 
 start.addEventListener("click", timer);
